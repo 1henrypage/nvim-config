815f099068ff5c6ceda228e0850c85c489bac802
Henry Page <h.page@student.tudelft.nl>
2025-05-07 15:26:38 +0200

    notify config

--- lua/1henrypage/plugins/mini.lua
+++ lua/1henrypage/plugins/mini.lua
@@ -1,5 +1,4 @@
-local comment_key = '<C-_>'
-
+local comment_key = "<C-_>"
 
 local comment_config = {
     mappings = {
@@ -12,10 +11,47 @@ local comment_config = {
 
 local split_join_config = {
     mappings = {
-        toggle = '<leader>J'
-    }
+        toggle = "<leader>J",
+    },
 }
 
+local notify_config = {
+    -- Content management
+    content = {
+        -- Function which orders notification array from most to least important
+        -- By default orders first by level and then by update timestamp
+        sort = function(notif_arr)
+            table.sort(notif_arr, function(a, b)
+                return a.ts_update > b.ts_update
+            end)
+            return notif_arr
+        end,
+    },
+
+    -- Notifications about LSP progress
+    lsp_progress = {
+        -- Whether to enable showing
+        enable = true,
+
+        -- Notification level
+        level = "ERROR",
+
+        -- Duration (in ms) of how long last message should be shown
+        duration_last = 3000,
+    },
+
+    -- Window options
+    window = {
+        -- Floating window config
+        config = {},
+
+        -- Maximum window width as share (between 0 and 1) of available columns
+        max_width_share = 0.382,
+
+        -- Value of 'winblend' option
+        winblend = 25,
+    },
+}
 
 return {
     {
@@ -24,10 +60,10 @@ return {
         config = function(_, opts)
             require("mini.comment").setup(comment_config)
             require("mini.pairs").setup({})
-            require("mini.notify").setup({})
+            require("mini.notify").setup(notify_config)
             require("mini.bracketed").setup({})
             require("mini.starter").setup({})
             require("mini.splitjoin").setup(split_join_config)
-        end
+        end,
     },
 }
