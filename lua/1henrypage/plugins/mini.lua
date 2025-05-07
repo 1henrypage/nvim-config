a1f0246fb5e2840cb784810bbeb6b67fec97cf65
Henry Page <h.page@student.tudelft.nl>
2025-05-07 16:33:00 +0200

    ai config

--- lua/1henrypage/plugins/mini.lua
+++ lua/1henrypage/plugins/mini.lua
@@ -1,5 +1,6 @@
 local comment_key = "<C-_>"
 
+
 local comment_config = {
     mappings = {
         comment = comment_key,
@@ -14,6 +15,42 @@ local split_join_config = {
         toggle = "<leader>J",
     },
 }
+
+local ai_config = {
+    -- Table with textobject id as fields, textobject specification as values.
+    -- Also use this to disable builtin textobjects. See |MiniAi.config|.
+    custom_textobjects = nil,
+
+    -- Module mappings. Use `''` (empty string) to disable one.
+    mappings = {
+      -- Main textobject prefixes
+      around = 'a',
+      inside = 'i',
+
+      -- Next/last textobjects
+      around_next = 'an',
+      inside_next = 'in',
+      around_last = 'al',
+      inside_last = 'il',
+
+      -- Move cursor to corresponding edge of `a` textobject
+      goto_left = 'g[',
+      goto_right = 'g]',
+    },
+
+    -- Number of lines within which textobject is searched
+    n_lines = 50,
+
+    -- How to search for object (first inside current line, then inside
+    -- neighborhood). One of 'cover', 'cover_or_next', 'cover_or_prev',
+    -- 'cover_or_nearest', 'next', 'prev', 'nearest'.
+    search_method = 'cover_or_next',
+
+    -- Whether to disable showing non-error feedback
+    -- This also affects (purely informational) helper messages shown after
+    -- idle time if user input is required.
+    silent = false,
+  }
 
 local notify_config = {
     -- Content management
@@ -53,11 +90,15 @@ local notify_config = {
     },
 }
 
+
+
+
 return {
     {
         "echasnovski/mini.nvim",
         version = "*",
         config = function(_, opts)
+            require("mini.ai").setup({ai_config})
             require("mini.comment").setup(comment_config)
             require("mini.pairs").setup({})
             require("mini.notify").setup(notify_config)
@@ -67,3 +108,4 @@ return {
         end,
     },
 }
+
